b4e154ac09784fc273b963990f5865e531d93016
Cecilia Barranco <cc94barranco@gmail.com>
2025-09-18 19:45:39 -0600

    feat: add html and second day of code in js file

--- 7DaysOfCode.js
+++ 7DaysOfCode.js
@@ -15,8 +15,6 @@ let numeroDiez = 10
 let stringDiez = '10'
 
 
-
-
 if (numeroUn == stringUn) {
 
   console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
@@ -49,4 +47,48 @@ if (numeroDiez == stringDiez) {
 
   console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
 
+}
+
+//Dia 2
+/* desarrollar un programa simulando una de esas aplicaciones. Debe pedir al usuario responder 3 preguntas:
+
+- ¿Cuál es tu nombre?
+- ¿Cuántos años tienes?
+- ¿Qué lenguaje de programación estás estudiando?
+
+A medida que se hagan las preguntas, la persona que esté usando el programa debe responder cada una de ellas. Al final, el sistema mostrará el mensaje:
+
+"Hola [nombre], tienes [edad] años y ya estás aprendiendo [lenguaje]!"
+*/
+
+const nombre = prompt("¿Cuál es tu nombre?");
+const edad = prompt("¿Cuántos años tienes?");
+const lenguajeProgamacion = prompt("¿Qué lenguaje de programación estás estudiando?");
+
+const msg = `Hola ${nombre}!, tienes ${edad} años y ya estás aprendiendo ${lenguajeProgamacion}!`;
+
+alert(msg);
+
+
+/* EJERCICIO OPCIONAL:
+Vas a complementar el código para que, después de mostrar el mensaje anterior, el programa pregunte:
+¿Te gusta estudiar [lenguaje]? Responde con el número 1 para SÍ o 2 para NO.
+
+Y luego, dependiendo de la respuesta, debería mostrar uno de los siguientes mensajes:
+
+1 > ¡Muy bien! Sigue estudiando y tendrás mucho éxito.
+2 > Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?
+
+*/
+
+const respuesta = prompt(`¿Te gusta estudiar ${lenguajeProgamacion}? Responde con el número 1 para SÍ o 2 para NO.`);
+
+if (respuesta == 1) {
+    // da la respuesta positiva
+  alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.");
+}
+
+if (respuesta == 2) {
+    // da la respuesta negativa
+    alert("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?");
 }
