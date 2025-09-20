5b3201318158dc808452240009c127328494f23d
Cecilia Barranco <cc94barranco@gmail.com>
2025-09-19 19:47:07 -0600

    feat: add dia 3 y botones en html

--- 7DaysOfCode.js
+++ 7DaysOfCode.js
@@ -2,18 +2,14 @@
 /*tu tarea de hoy es reescribir el código a continuación para que imprima la información de manera correcta, que tenga sentido y sin errores:
 Si deseas cambiar los nombres de las variables y los valores, siéntete libre de hacerlo. Pero nunca imprimas algo que no sea verdadero, ¿eh?
 */
+
+/*
 let numeroUn = 1
-
 let stringUn = '1'
-
 let numeroTreinta = 30
-
 let stringTreinta = '30'
-
 let numeroDiez = 10
-
 let stringDiez = '10'
-
 
 if (numeroUn == stringUn) {
 
@@ -25,7 +21,6 @@ if (numeroUn == stringUn) {
 
 }
 
- 
 
 if (numeroTreinta === stringTreinta) {
 
@@ -37,7 +32,6 @@ if (numeroTreinta === stringTreinta) {
 
 }
 
- 
 
 if (numeroDiez == stringDiez) {
 
@@ -48,6 +42,7 @@ if (numeroDiez == stringDiez) {
   console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
 
 }
+*/
 
 //Dia 2
 /* desarrollar un programa simulando una de esas aplicaciones. Debe pedir al usuario responder 3 preguntas:
@@ -61,6 +56,7 @@ A medida que se hagan las preguntas, la persona que esté usando el programa deb
 "Hola [nombre], tienes [edad] años y ya estás aprendiendo [lenguaje]!"
 */
 
+/*
 const nombre = prompt("¿Cuál es tu nombre?");
 const edad = prompt("¿Cuántos años tienes?");
 const lenguajeProgamacion = prompt("¿Qué lenguaje de programación estás estudiando?");
@@ -68,6 +64,7 @@ const lenguajeProgamacion = prompt("¿Qué lenguaje de programación estás estu
 const msg = `Hola ${nombre}!, tienes ${edad} años y ya estás aprendiendo ${lenguajeProgamacion}!`;
 
 alert(msg);
+*/
 
 
 /* EJERCICIO OPCIONAL:
@@ -78,9 +75,9 @@ Y luego, dependiendo de la respuesta, debería mostrar uno de los siguientes men
 
 1 > ¡Muy bien! Sigue estudiando y tendrás mucho éxito.
 2 > Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?
-
 */
 
+/*
 const respuesta = prompt(`¿Te gusta estudiar ${lenguajeProgamacion}? Responde con el número 1 para SÍ o 2 para NO.`);
 
 if (respuesta == 1) {
@@ -92,3 +89,46 @@ if (respuesta == 2) {
     // da la respuesta negativa
     alert("Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?");
 }
+*/
+
+//Dia 3
+/*Tu desafío de hoy es crear los destinos posibles de un juego, en el que el usuario pueda elegir:
+
+1. Si quiere seguir hacia el área de Front-End o seguir hacia el área de Back-End.
+ 
+2. Si está en el área de Front-End, si quiere aprender React o aprender Vue. Si está en el área de Back-End, podrá aprender  o aprender Java.
+ 
+3. Después, independientemente de las elecciones anteriores, el usuario podrá elegir entre seguir especializándose en el área elegida o desarrollarse para convertirse en Fullstack. Debes mostrar en pantalla un mensaje específico para cada elección.
+ 
+4. Finalmente, pregunta en qué tecnologías le gustaría a la persona especializarse o conocer. Aquí, la persona puede responder N tecnologías, una a la vez. Entonces, mientras continúe respondiendo **ok** a la pregunta: "¿Hay alguna otra tecnología que te gustaría aprender?", sigue presentando el Prompt, para que complete el nombre de la tecnología en cuestión. Y, justo después, presenta un mensaje comentando algo sobre la tecnología ingresada.
+*/
+
+//let respuestaUno = prompt("¿quieres seguir hacia el área de Front-End, hacia el área de Back-End o prefieres seguir hacia el área Fullstack?");
+
+function preguntaUno () {
+  respuestaUno = prompt("¿quieres seguir hacia el área de Front-End, hacia el área de Back-End o prefieres seguir hacia el área Fullstack?");
+}
+
+function preguntaDos () {
+    if (respuestaUno == "Front-End") {
+    respuestaDos = prompt("¿quieres aprender React o aprender Vue?")
+    alert("Suerte en tu camino Front-End")
+    } else if (respuestaUno == "Back-End") {
+      respuestaDos = prompt("¿quieres aprender C# o Java")
+      alert("Suerte en tu camino Back-End")  
+    } else if (respuestaUno == "Fullstack"){
+      alert("Suerte en tu camino Fullstack")
+    } else {
+      alert("Elige un camino ninja válido")
+    }
+}
+
+function preguntaAdicional () {
+  respuestaTres = prompt("¿Hay alguna otra tecnología que te gustaría aprender?");
+  if (respuestaTres == "no") {
+    alert("oki")
+  } else {
+    respuestaCuatro = prompt("¿Escribe que tecnologías te gustaría aprender?");
+  }
+} 
+
