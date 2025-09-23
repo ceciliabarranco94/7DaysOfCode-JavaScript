01526a53268a0319e54ecfeaed395caf0471e233
Cecilia Barranco <cc94barranco@gmail.com>
2025-09-22 20:23:31 -0600

    fix: h1 y h3

--- 7DaysOfCode.js
+++ 7DaysOfCode.js
@@ -221,7 +221,7 @@ function generarNumeroSecreto() {
 }
 
 function condicionesIniciales() {
-    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('h3','Juego del número secreto!');
     asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
